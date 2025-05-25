a8ef1082c58111bb7435dd4ee7058889d5f114cf
alchemistcohen <alancohen7@gmail.com>
2025-05-24 23:43:59 -0400

    fix: conflicto resuelto en vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-<<<<<<< HEAD
   base: "/e-plantShopping/",
-=======
->>>>>>> 562ae2c7e649435125417cc53a0f2768db37aee1
   plugins: [react()],
-  base: '/e-plantShopping/', // 👈 muy importante
 })
-
